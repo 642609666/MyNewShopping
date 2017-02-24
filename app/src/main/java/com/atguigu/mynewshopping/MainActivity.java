d665ed1a19a4276cfdb53b982fff3275fff48f9e
642609666 <642609666@qq.com>
2017-02-24 15:00:25 +0800

    主界面简单完成

--- app/src/main/java/com/atguigu/mynewshopping/MainActivity.java
+++ app/src/main/java/com/atguigu/mynewshopping/MainActivity.java
@@ -1,13 +1,29 @@
 package com.atguigu.mynewshopping;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @InjectView(R.id.rl_main)
+    RelativeLayout rlMain;
+    @InjectView(R.id.rg_main)
+    RadioGroup rgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        rgMain.check(R.id.rb_home);
+
+
     }
 }
